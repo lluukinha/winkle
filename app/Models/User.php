71018cdda44369c4de589ee444dbeb390abf8e41
Lucas <lucas.prog07@gmail.com>
2022-01-24 16:44:33 -0300

    Create passwords migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -62,4 +62,8 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
+    public function passwords() {
+        return $this->belongsToMany(Password::class);
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
         User::create([
             'name' => 'Lucas Souza',
             'email' => 'lucas.prog07@gmail.com',
